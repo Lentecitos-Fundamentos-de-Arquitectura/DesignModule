45b5653c59f7f2816b932649a3493868d6f0249c
The Editor <68383325+Marpellic@users.noreply.github.com>
2024-05-03 21:54:46 -0500

    FEAT: Implmented API Controllers
    Implemented following classes:
    ColorController.java
    DisenoAnteojosController.java
    MarcoAnteojosController.java
    MaterialFabricaLunaController.java
    MaterialFabricacionMonturaController.java
    PatasAnteojosController.java

--- src/main/java/com/lentecitos/design/design/design/api/ColorController.java
+++ src/main/java/com/lentecitos/design/design/design/api/ColorController.java
@@ -50,7 +50,7 @@ public class ColorController {
 
     @GetMapping
     public ResponseEntity<List<Color>>  fetchAll(){
-        return ResponseEntity.ok(colorService.findAll());
+        return ResponseEntity.ok(colorService.findAll())
     }
 
 }
